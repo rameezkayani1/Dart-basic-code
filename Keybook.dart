06cc3981e5930b30075bd6f4bd293909c8a3c196
RAMEEZ KAYANI <101308522+rameezkayani1@users.noreply.github.com>
2022-03-12 09:22:57 -0800

    Add files via upload

---
+++ Keybook.dart
@@ -0,0 +1,50 @@
+main() {
+  /* double kilometer = 1.609;
+  double miles = 25;
+  double distance = kilometer * miles;
+  print("total Miles is $distance");
+  */
+  // Question nNo.5
+  final ali = 25;
+  final saqib = 36;
+  final raza = 35;
+  final usman = 17;
+  final totalscore = ali + saqib + raza + usman;
+  print("The sum of all Player Score is $totalscore");
+  final theproduct = ali * saqib * raza * usman;
+  print("The product of all Player $theproduct");
+  final average = totalscore / 4;
+  print("The average Score of all player is $average");
+  // Qestion no 6
+  final age = 23;
+  final month = 23 * 12;
+  print("My age in Month is $month");
+  final days = age * 365;
+  print("My age in Days $days");
+  // Question 7
+  final square = 25;
+  final Tsquare = square * square;
+  print("The Square re of $square is $Tsquare ");
+
+  // Question no 8
+
+  // Question no 9
+  final petrol = 12;
+  final distance = 52;
+  final totalDistance = petrol * distance;
+  print(
+      "I have ${petrol}L Gas and i covered & travled distance is ${totalDistance}KM. ");
+
+  //  Question no 10
+  final totalSudent = 25;
+  final Sfees = 50;
+  final collectedFess = totalSudent * Sfees;
+  print(
+      "Total Student of Class is $totalSudent and per student fee is $Sfees  . The collected fee is $collectedFess");
+
+  // Question no 11
+
+  final ftemeperture = 25;
+  final Celsius = 5 / 9 * (ftemeperture - 32);
+  print("The termperature in Celsius is $Celsius");
+}
